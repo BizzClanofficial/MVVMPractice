c3c4cade14f3ea113b91fb8f9c43a989ae1607dd
BizzClan <junaidmalik.787898@gmail.com>
2022-06-14 14:57:24 +0500

    Initial Commit

---
+++ MVVMPractice/MVVMPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMPracticeApp.swift
+//  MVVMPractice
+//
+//  Created by Junaid Malik on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MVVMPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
